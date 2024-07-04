9629757c92447007f45a5848949ac39866c73a3c
HIGOR EDUARDO DA SILVA <higor_e_silva@estudante.sesisenai.org.br>
2024-07-04 09:15:05 -0300

    prova

---
+++ provaPratica/src/components/Principal.jsx
@@ -0,0 +1,14 @@
+import Header from "./Cabeca"
+import Clubes from "./Lista"
+
+export default function Home(){
+return(
+<>
+<div className="App">
+<Header nome="Higor Eduardo" Turma="Turma: 3B" />
+<Clubes />
+</div>
+</>
+)
+}
+
